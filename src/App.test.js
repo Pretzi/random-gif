2552b2889375bb0c87ec2d4d8e3124f8f882e268
Pretzi <manuel@ciudaliapp.com>
2018-05-05 16:00:55 -0500

    Add firs test for <App/>

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  it('Should render as expected', () => {
+    const wrapper = shallow(
+      <App />
+    );
+    expect(wrapper).toMatchSnapshot();
+  })
 });
