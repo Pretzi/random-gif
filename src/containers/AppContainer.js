97714496ac878df2d84cbba8177dd38502e03d2a
Pretzi <manuel@ciudaliapp.com>
2018-05-05 16:40:18 -0500

    Add redux container for main App component

---
+++ src/containers/AppContainer.js
@@ -0,0 +1,30 @@
+import { connect } from 'react-redux';
+import { gifsActions } from '../actions/gifs-actions'
+import App from '../App';
+
+const {
+  FETCH_GIFS
+} = gifsActions;
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    gifs: state.gifs.entities
+  }
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    fetchGifs(category, numberOfImages, timer) {
+      dispatch({
+        type: FETCH_GIFS,
+        payload: {
+          category,
+          numberOfImages,
+          timer
+        }
+      });
+    }
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
