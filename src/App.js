4e71021cae1ffd0744561fb6c2c85d2ebceff626
Pretzi <manuel@ciudaliapp.com>
2018-05-05 16:14:21 -0500

    Add timer input

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      category: ''
+      category: '',
+      timer: 0
     };
   }
 
@@ -23,17 +24,29 @@ class App extends Component {
       <div className="App">
         <div className="App__container">
           <h2 className="App__title">Search Random Gifs</h2>
-          <FormGroup
-            label="Gif Category"
-          >
+
+          <FormGroup label="Gif Category">
             <Input 
               value={this.state.category} 
               onChange={e => this.handleInputChange(e)}
               placeholder="Tacos"
               type="search"
-              name='category'
+              name="category"
+              icon="pt-icon-search"
             /> 
           </FormGroup>
+
+          <FormGroup label="Timer">
+            <Input 
+              value={this.state.timer} 
+              onChange={e => this.handleInputChange(e)}
+              placeholder="10 seconds"
+              name="timer"
+              type="number"
+              icon="pt-icon-time"
+            /> 
+          </FormGroup>
+
         </div>
       </div>
     );
