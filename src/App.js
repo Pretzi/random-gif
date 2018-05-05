4540d065ee27dbcb86bd00820de14fdb8730acda
Pretzi <manuel@ciudaliapp.com>
2018-05-05 15:21:59 -0500

    Add initial Input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { FormGroup } from "@blueprintjs/core";
+
 import './App.css';
 
 class App extends Component {
@@ -7,6 +9,14 @@ class App extends Component {
       <div className="App">
         <div className="App__container">
           <h2 className="App__title">Search Random Gifs</h2>
+          <FormGroup
+            label="Gif Category"
+          >
+            <div className="pt-input-group pt-large">
+              <span className="pt-icon pt-icon-search"></span>
+              <input className="pt-input pt-large" type="search" placeholder="Tacos" dir="auto" />
+            </div>
+          </FormGroup>
         </div>
       </div>
     );
