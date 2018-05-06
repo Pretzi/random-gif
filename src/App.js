7f426d34c59c1ef3300fbf5f749c40139f749275
Pretzi <manuel@ciudaliapp.com>
2018-05-05 20:55:01 -0500

    Add missing class and type

--- src/App.js
+++ src/App.js
@@ -60,50 +60,54 @@ class App extends Component {
       <div className="App">
         <div className="App__container">
           <h2 className="App__title">Search Random Gifs</h2>
-          <form onSubmit={this.handleSubmit}>
-          <FormGroup label="Gif Category">
-            <Input
-              value={this.state.category}
-              onChange={e => this.handleInputChange(e)}
-              placeholder="Tacos"
-              type="string"
-              name="category"
-              icon="pt-icon-tag"
+          <form
+            className="App__form"
+            onSubmit={this.handleSubmit}
+          >
+            <FormGroup label="Gif Category">
+              <Input
+                value={this.state.category}
+                onChange={e => this.handleInputChange(e)}
+                placeholder="Tacos"
+                type="string"
+                name="category"
+                icon="pt-icon-tag"
+              />
+            </FormGroup>
+
+            {showNumberOfImages &&
+              <FormGroup label="Number Of Images">
+                <Input
+                  value={this.state.numberOfImages}
+                  onChange={e => this.handleInputChange(e)}
+                  placeholder="3"
+                  name="numberOfImages"
+                  type="number"
+                  icon="pt-icon-media"
+                />
+              </FormGroup>
+            }
+
+            {showTimer &&
+              <FormGroup label="Timer" helperText="Time is in seconds">
+                <Input
+                  value={this.state.timer}
+                  onChange={e => this.handleInputChange(e)}
+                  placeholder="10 seconds"
+                  name="timer"
+                  type="number"
+                  icon="pt-icon-time"
+                />
+              </FormGroup>
+            }
+
+            <Button
+              intent="PRIMARY"
+              text="Search"
+              large
+              onClick={this.handleSubmit}
+              type="submit"
             />
-          </FormGroup>
-
-          {showNumberOfImages &&
-            <FormGroup label="Number Of Images">
-              <Input
-                value={this.state.numberOfImages}
-                onChange={e => this.handleInputChange(e)}
-                placeholder="3"
-                name="numberOfImages"
-                type="number"
-                icon="pt-icon-media"
-              />
-            </FormGroup>
-          }
-
-          {showTimer &&
-            <FormGroup label="Timer" helperText="Time is in seconds">
-              <Input
-                value={this.state.timer}
-                onChange={e => this.handleInputChange(e)}
-                placeholder="10 seconds"
-                name="timer"
-                type="number"
-                icon="pt-icon-time"
-              />
-            </FormGroup>
-          }
-
-          <Button
-            intent="PRIMARY"
-            text="Search"
-            large
-            onClick={this.handleSubmit}
-          />
           </form>
 
           <div className="App__gif-container">
