faa7d4708a7f81a28d061c4376b2f4e52c49a5ac
Pretzi <manuel@ciudaliapp.com>
2018-05-05 23:45:28 -0500

    Change wording of actions

--- src/actions/gifs-actions.js
+++ src/actions/gifs-actions.js
@@ -1,8 +1,8 @@
 export const gifsActions = {
-  FETCH_GIFS: '[Gifs] Fetch Gifs',
-  FETCH_GIFS_SUCCESS: '[Gifs] Fetch Gifs Success',
-  FETCH_GIFS_ERROR: '[Gifs] Fetch Gifs Error',
+  FETCH_GIFS: '[Gifs] Fetch Gif',
+  FETCH_GIFS_SUCCESS: '[Gifs] Fetch Gif Success',
+  FETCH_GIFS_ERROR: '[Gifs] Fetch Gif Error',
   CLEAR_GIFS: '[Gifs] Clear Gifs',
-  FETCH_GIFS_CANCEL: '[Gifs] Fetch Gifs Cancel',
+  FETCH_GIFS_CANCEL: '[Gifs] Fetch Gif Cancel',
   GIFS_NOT_FOUND: '[Gifs] not found'
 };
