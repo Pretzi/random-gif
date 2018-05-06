b46817150d2a920030e41cd4b67df64fc4800087
Pretzi <manuel@ciudaliapp.com>
2018-05-05 20:55:11 -0500

    Remove unused action

--- src/actions/gifs-actions.js
+++ src/actions/gifs-actions.js
@@ -2,6 +2,5 @@ export const gifsActions = {
   FETCH_GIFS: '[Gifs] Fetch Gifs',
   FETCH_GIFS_SUCCESS: '[Gifs] Fetch Gifs Success',
   FETCH_GIFS_ERROR: '[Gifs] Fetch Gifs Error',
-  CLEAR_GIFS: '[Gifs] Clear Gifs',
-  FETCH_GIFS_CANCEL: '[Gifs] Fetch Gifs Cancel'
+  CLEAR_GIFS: '[Gifs] Clear Gifs'
 };
